030ebca4525c8da0ae14f2b0fba42b28a7c9b388
Jaeseok Choi <cam_choi9@gwu.edu>
2025-07-01 23:38:47 -0500

    Setup Plaid Link integration and backend /plaid/create_link_token endpoint

--- backend/config.py
+++ backend/config.py
@@ -1,10 +1,14 @@
 from pydantic_settings import BaseSettings
+from pydantic import Field
 
 class Settings(BaseSettings):
     DATABASE_URL: str
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
+    PLAID_CLIENT_ID: str = Field(..., env="PLAID_CLIENT_ID")
+    PLAID_SECRET: str = Field(..., env="PLAID_SECRET")
+    ENV: str
 
     class Config:
         env_file = ".env"
--- backend/main.py
+++ backend/main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from database import SessionLocal, engine, Base
 from auth.routes import router as auth_router
+from api.plaid import router as plaid_router
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.include_router(auth_router, prefix='/api')
+app.include_router(plaid_router, prefix="/api")
 
 # allows frontend to call backend without CORS error
 app.add_middleware(
